6c2a57a720df72a57c1b61ac3c64cab87bb0c571
mind <mind@minds-MacBook-Pro.local>
2024-04-19 17:45:04 -0600

    name change

--- db/migrate/20240419210053_create_stops.rb
+++ db/migrate/20240419210053_create_stops.rb
@@ -2,7 +2,7 @@ class CreateStops < ActiveRecord::Migration[7.1]
   def change
     create_table :stops do |t|
       t.string :name
-      t.column :trayectory_geom, :geometry, limit: "POINT"
+      t.column :trayectory_geom :geometry limit: "POINT"
       t.string :address
       t.timestamps
     end
