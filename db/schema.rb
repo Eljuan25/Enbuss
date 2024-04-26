9385da74a48c612e018f2e47f830708125601cfa
Manuel Topete <mtopete.dev@gmail.com>
2024-04-26 17:50:14 -0600

    Adding NOT NULL constraints to Trajectories table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_26_221609) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_25_224245) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "postgis"
@@ -27,31 +27,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_26_221609) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "stops", force: :cascade do |t|
-    t.string "name"
-    t.geometry "trayectory_geom", limit: {:srid=>0, :type=>"geometry"}
-    t.string "address"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "stops_cities", force: :cascade do |t|
-    t.bigint "stop_id"
-    t.bigint "city_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["city_id"], name: "index_stops_cities_on_city_id"
-    t.index ["stop_id"], name: "index_stops_cities_on_stop_id"
-  end
-
   create_table "trajectories", force: :cascade do |t|
-    t.string "name"
-    t.time "passing_frequency"
-    t.integer "estimated_time"
-    t.integer "service_cost"
-    t.time "start_time"
-    t.time "end_time"
-    t.point "trajectory_point"
+    t.string "name", null: false
+    t.time "passing_frequency", null: false
+    t.integer "estimated_time", null: false
+    t.integer "service_cost", null: false
+    t.time "start_time", null: false
+    t.time "end_time", null: false
+    t.point "trajectory_point", null: false
     t.bigint "cities_id", null: false
     t.bigint "services_id", null: false
     t.datetime "created_at", null: false
@@ -67,18 +50,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_26_221609) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "units", force: :cascade do |t|
-    t.string "name"
-    t.integer "capacity"
-    t.bigint "type_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["type_id"], name: "index_units_on_type_id"
-  end
-
-  add_foreign_key "stops_cities", "cities"
-  add_foreign_key "stops_cities", "stops"
   add_foreign_key "trajectories", "cities", column: "cities_id"
   add_foreign_key "trajectories", "services", column: "services_id"
-  add_foreign_key "units", "types"
 end
