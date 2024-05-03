5feece3adef0a04c9e352e897ddf9ba852db1e49
RichMicke <richardpuga17@gmail.com>
2024-05-03 15:15:26 -0600

    The city_stops model was created, it still needs to be finished and added values

--- app/models/city_stop.rb
+++ app/models/city_stop.rb
@@ -2,7 +2,5 @@ class CityStop < ApplicationRecord
     belongs_to :stop
     belongs_to :city
       
-    validates :stop_id, :city_id, presence: true
-
-    end  
+    validates :stop_id, :city_id, presence: true  
 end
