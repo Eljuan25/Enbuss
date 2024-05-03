23adae67c3df8dee9331ec845159e14dc22a15e4
JuanAlon <elbartoy@gmail.com>
2024-05-03 15:02:04 -0600

    This is the service correction since I put it in plural.

--- app/models/trajectory.rb
+++ app/models/trajectory.rb
@@ -1,7 +1,7 @@
 class Trajectory < ApplicationRecord
     has_and_belongs_to_many :stops
     belongs_to :city
-    belongs_to :services
+    belongs_to :service
 
     validates :city_id, presence: true 
     validates :services_id, presence: true 
