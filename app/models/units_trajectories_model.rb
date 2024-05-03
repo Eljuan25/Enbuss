fb23095704e61750f0157318a68e4ee3675785dd
RichMicke <richardpuga17@gmail.com>
2024-05-03 17:48:40 -0600

    resolving comments

--- app/models/units_trajectories_model.rb
+++ app/models/units_trajectories_model.rb
@@ -1,5 +1,4 @@
 class UnitsTrajectoriesModel < ApplicationRecord
-    class UnitsTrajectory < ApplicationRecord
     belongs_to :unit
     belongs_to :trajectory
       
--- db/migrate/20240503213904_create_units_trajectories_models.rb
+++
@@ -1,8 +0,0 @@
-class CreateUnitsTrajectoriesModels < ActiveRecord::Migration[7.1]
-  def change
-    create_table :units_trajectories_models do |t|
-
-      t.timestamps
-    end
-  end
-end
