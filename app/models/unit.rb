1e33e0a8e8497ddd74b99d66357659372bbb041e
reinavallin <reynavallin94@gmail.con>
2024-05-07 17:38:42 -0600

    resolved unit model comment

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -1,6 +1,6 @@
 class Unit < ApplicationRecord
-    has_one : trajectories
-    has_many : type 
+    has_one :trajectory
+    has_many :types 
 
     validates :name, presence: true, length: { maximum: 50 }
     validates :capacity, presence: true
