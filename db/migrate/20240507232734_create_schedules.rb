39337ce9699769c79f9f604530edc34b2e66cc99
Eljuan25 <147083751+Eljuan25@users.noreply.github.com>
2024-05-07 17:44:18 -0600

    This schedulesModelCreate (#21)
    * this is creation about Models and Migration of schedule that includes belongs_to and has_many in the model trayectory
    
    * this changes a schedule a schedules

---
+++ db/migrate/20240507232734_create_schedules.rb
@@ -0,0 +1,11 @@
+class CreateSchedules < ActiveRecord::Migration[7.1]
+  def change
+    create_table :schedules do |t|
+      t.time :time
+      t.references :trayectories, null: false, foreign_key: true
+      t.references :stop, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
