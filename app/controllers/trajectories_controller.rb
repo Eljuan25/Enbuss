67dd18a20b8cc67f95333df6a9606ef530b31d1e
JuanAlon <elbartoy@gmail.com>
2024-05-09 13:51:46 -0600

    add the destroy and update also the changes they asked me for

--- app/controllers/trajectories_controller.rb
+++ app/controllers/trajectories_controller.rb
@@ -1,8 +1,15 @@
 class TrajectoriesController < ApplicationController
     before_action :set_trajectory, only [show update destroy]
     
-    #met Get
-    def
+    # Get
+    def index 
+        @trajectories = Trajectory.all 
+        render json: @trajectories
+    end
+
+    
+    
+    def show
         @trajectory = Trajectory.indCluides(:schedules). find(parms [:id])
         render json: @trajectory.to_json(include: :schedules)
     end
@@ -19,14 +26,40 @@ class TrajectoriesController < ApplicationController
     end
     # Use callbacks to share common setup or constraints between actions.
 
+    def update
+        if @trajectory.update(trajectoy_params)
+            render jscon: @trayectory
+        else
+            render json @trajectory.errors, status: :unprocessable_entity
+        end
+    end
+
+
+    def destroy
+        @trajectory.destroy
+        head :no_content
+    end
+
+
+
     private 
 
     def set_trajectory
         @trayectory = Trajectory.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+        render json: {error: 'Trayectory not found'},status: :not_found 
     end
 
     def trajectoy_params
-        params.require(:trajectory).permit(:name, :passing_frequency, :estimated_time, :service_cost, :start_time, :end_time, :trajectory_point, :cities_id, :services_id)
+        params.require(:trajectory).permit(:name, 
+        :passing_frequency, 
+        :estimated_time, 
+        :service_cost, 
+        :start_time, 
+        :end_time, 
+        :trajectory_point, 
+        :cities_id, 
+        :services_id)
     end
 end
 
--- app/helpers/trajectories_helper.rb
+++
@@ -1,2 +0,0 @@
-module TrajectoriesHelper
-end
