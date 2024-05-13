6c4dbd2c783a5a37c2498b7d925f860779d39bf5
JuanAlon <elbartoy@gmail.com>
2024-05-13 16:48:26 -0600

    I corrected my typos for example my trajectoy by trajectory

--- app/controllers/trajectories_controller.rb
+++ app/controllers/trajectories_controller.rb
@@ -1,67 +1,58 @@
 class TrajectoriesController < ApplicationController
     before_action :set_trajectory, only: %i[show update destroy]
-    
-    # Get
-    def index 
-        @trajectories = Trajectory.all 
-        render json: @trajectories
+  
+    def index
+      @trajectories = Trajectory.all
+      render json: @trajectories.to_json(include: :schedules)
     end
-
-    
-    
+  
     def show
-        @trajectory = Trajectory.indCluides(:schedules). find(parms [:id])
-        render json: @trajectory.to_json(include: :schedules)
+      @trajectory = Trajectory.includes(:schedules).find(params[:id])
+      render json: @trajectory.to_json(include: :schedules)
     end
-
-    #post /trajectories 
-
+  
     def create
-        @trajectory = Trajectory.new(trajectoy_params)
-        if @trajectory.save
-            render json: @trajectory, status: :created
-        else 
-            render json: @trajectory.errors status: :unprocessable_entity
-        end
+      @trajectory = Trajectory.new(trajectory_params)
+      if @trajectory.save
+        render json: @trajectory, status: :created
+      else
+        render json: @trajectory.errors, status: :unprocessable_entity
+      end
     end
-    # Use callbacks to share common setup or constraints between actions.
-
+  
     def update
-        if @trajectory.update(trajectoy_params)
-            render jscon: @trayectory
-        else
-            render json @trajectory.errors, status: :unprocessable_entity
-        end
+      if @trajectory.update(trajectory_params)
+        render json: @trajectory
+      else
+        render json: @trajectory.errors, status: :unprocessable_entity
+      end
     end
-
-
+  
     def destroy
-        @trajectory.destroy
-        head :no_content
+      @trajectory.destroy
+      head :no_content
     end
-
-
-
-    private 
-
+  
+    private
+  
     def set_trajectory
-        @trayectory = Trajectory.find(params[:id])
+      @trajectory = Trajectory.find(params[:id])
     rescue ActiveRecord::RecordNotFound
-        render json: {error: 'Trayectory not found'},status: :not_found 
+      render json: { error: 'Trajectory not found' }, status: :not_found
     end
-
-    def trajectoy_params
-        params.require(:trajectory).permit(:name, 
-        :passing_frequency, 
-        :estimated_time, 
-        :service_cost, 
-        :start_time, 
-        :end_time, 
-        :trajectory_point, 
-        :cities_id, 
-        :services_id)
+  
+    def trajectory_params
+      params.require(:trajectory).permit(
+        :name,
+        :passing_frequency,
+        :estimated_time,
+        :service_cost,
+        :start_time,
+        :end_time,
+        :trajectory_point,
+        :cities_id,
+        :services_id
+      )
     end
-end
-
-
-#UWU......Uwu
+  end
+  
