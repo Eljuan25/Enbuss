576491ebc5357c008cac4c65addac04f36386681
DanielaAgustin <daniiela508@gmail.com>
2024-05-14 18:18:23 -0500

    just add the resources and put a space in params

--- app/controllers/trajectories_controller.rb
+++ app/controllers/trajectories_controller.rb
@@ -1,4 +1,4 @@
 class TrajectoriesController < ApplicationController
   def index 
-    Trajectory.where(city_id:params.fetch(:city_id,[]))
+    Trajectory.where(city_id: params.fetch(:city_id,[]))
   end
