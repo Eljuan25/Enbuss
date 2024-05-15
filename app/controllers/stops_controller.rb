df3d13c58ff65a6459280474cd392e74bfdd4934
JuanAlon <elbartoy@gmail.com>
2024-05-15 13:33:54 -0600

    resolve indentation conflicts in the stops_controller part, also add the :stops rosources, as a typo in the stops migration.

--- app/controllers/stops_controller.rb
+++ app/controllers/stops_controller.rb
@@ -1,63 +1,51 @@
 class StopsController < ApplicationController
-    before_action : set_stop, only: %i[show update destroy]
-
-      #Get
-      def index
-        @stop = Stop.all
-        render json: @stops
-      end
-
-      # GET /stops/1
-      def show
-        render json: {
-          id: @stop.id,
-          name: @stop.name,
-          other_attribute: @stop.other_attribute,
-          schedules: @stop.schedules.map { |schedule| { id: schedule.id, time: schedule.time } }}
-      end
-
-      # POST /stops
-      def create
-          @stop = Stop.new(stop_params)
-
-        if @stop.save
-            redirect_to @stop, notice: 'Stop was successfully created.'
-            else
-                render :new
-            end
-        end
+    before_action :set_stop, only: %i[show update destroy]
   
-
-        def update 
-            @stop = Stop.find(params[ :id])
-            if @stop.update(stop_params)
-                redirect_to @stop, notice: 'Stop was succefully updated.'
-            else 
-            render :edit
-            end        
-        end
-
-
-        def destroy 
-            @stop = Stop.find(params[:id])
-            @stop.destroy
-            redirect_to stops_url, notice: 'Stop was succefully updated.'
-        end
-
-
-        private
-
-        def set_stop
-            @stop = Stop.find(params[:id])
-        rescue ActiveRecord::RecordNotFound
-            render json: {error: 'Stop not found'},status: :not_found 
-        end
-
-            def stop_params
-                params.require(:stop).permit(
-                :name,
-                :trayectory_geom,
-                :address)
-            end 
-        end   
+    def show
+      render json: {
+        id: @stop.id,
+        name: @stop.name,
+        other_attribute: @stop.other_attribute,
+        schedules: @stop.schedules.map { |schedule| { id: schedule.id, time: schedule.time } }
+      }
     end
+
+    def create
+      @stop = Stop.new(stop_params)
+  
+      if @stop.save
+        render json: @stop, status: :created
+      else
+        render json: @stop.errors, status: :unprocessable_entity
+      end
+    end
+  
+    def update 
+      if @stop.update(stop_params)
+        render json: @stop
+      else 
+        render json: @stop.errors, status: :unprocessable_entity
+      end
+    end
+  
+    def destroy 
+      @stop.destroy
+      head :no_content
+    end
+  
+    private
+  
+    def set_stop
+      @stop = Stop.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      render json: { error: 'Stop not found' }, status: :not_found 
+    end
+  
+    def stop_params
+      params.require(:stop).permit(
+        :name,
+        :trajectory_geom,
+        :address
+      )
+    end 
+end
